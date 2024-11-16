1fab6cbbd881aed547679fa172b725a7a50ec26e
woohyun23 <parkwh0823@gmail.com>
2024-11-17 01:53:25 +0900

    [set] eclipse git연동

---
+++ java_team_project/src/java_team_project/ex.java
@@ -0,0 +1,8 @@
+package java_team_project;
+
+public class ex {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
---
+++ java_team_project/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module java_team_project {
+}
